f7699b8e62c12f4eef3cf7f742febe96f6c58f72
ilium007 <brantwinter@gmail.com>
2025-11-23 12:16:10 +1000

    Update gpg command to use --yes and --batch options

--- linux_bootstrap.sh
+++ linux_bootstrap.sh
@@ -28,7 +28,7 @@ rm -f /tmp/fastfetch.deb
 ##############################################
 mkdir -p /etc/apt/keyrings
 wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc \
-    | gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+    | gpg --yes --batch --dearmor -o /etc/apt/keyrings/gierens.gpg
 
 echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" \
     > /etc/apt/sources.list.d/gierens.list
