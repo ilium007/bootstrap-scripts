5fa243ae557259a640c106ad57da995fa86c988d
ilium007 <brantwinter@gmail.com>
2025-11-23 11:42:18 +1000

    Update linux_bootstrap.sh for temp file handling

--- linux_bootstrap.sh
+++ linux_bootstrap.sh
@@ -17,10 +17,10 @@ AGE_KEY_PATH="$HOME/.config/age/keys.txt"
 TAG=$(curl -s https://api.github.com/repos/fastfetch-cli/fastfetch/releases/latest \
     | grep -oP '"tag_name":\s*"\K(.*?)(?=")')
 
-curl -L -o fastfetch-linux-amd64.deb \
+curl -L -o /tmp/fastfetch-linux-amd64.deb \
     "https://github.com/fastfetch-cli/fastfetch/releases/download/${TAG}/fastfetch-linux-amd64.deb"
-sudo apt install -y ./fastfetch-linux-amd64.deb >/dev/null 2>&1 && \
-rm ./fastfetch-linux-amd64.deb >/dev/null 2>&1
+sudo apt install -y /tmp/fastfetch-linux-amd64.deb >/dev/null 2>&1 && \
+rm /tmp/fastfetch-linux-amd64.deb >/dev/null 2>&1
 
 # eza
 sudo mkdir -p /etc/apt/keyrings && \
@@ -31,10 +31,9 @@ sudo apt update && \
 sudo apt install -y eza
 
 # fzf
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install --all
+git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf && $HOME/.fzf/install --all
 
 # Starship
-#curl -sS https://starship.rs/install.sh | sh -s -- -y
 sudo sh -c 'curl -sS https://starship.rs/install.sh | sh -s -- -y >/dev/null 2>&1'
 
 # zap for zsh
@@ -47,8 +46,8 @@ if ! command -v chezmoi >/dev/null 2>&1; then
 fi
 
 # Clone chezmoi repo using bootstrap key
-mkdir -p ~/.local/share/chezmoi
-chmod 700 ~/.local/share/chezmoi
+mkdir -p $HOME/.local/share/chezmoi
+chmod 700 $HOME/.local/share/chezmoi
 echo "Cloning dotfiles repo using bootstrap key..."
 export GIT_SSH_COMMAND="ssh -i $BOOTSTRAP_KEY -o IdentitiesOnly=yes"
 chezmoi init "$REPO"
@@ -59,7 +58,7 @@ chezmoi apply
 
 # Switch chezmoi remote to permanent SSH key
 echo "Switching chezmoi repo to permanent SSH key..."
-cd ~/.local/share/chezmoi
+cd $HOME/.local/share/chezmoi
 unset GIT_SSH_COMMAND
 git remote set-url origin "$REPO"
 
@@ -68,5 +67,7 @@ curl -LsSf https://astral.sh/uv/install.sh | sh
 uv python install
 
 # Clean up bootstrap key
-echo "Cleaning up temporary bootstrap key..."
 rm -f "$BOOTSTRAP_KEY"
+
+# Complete
+echo "Bootstrap script complete..."
